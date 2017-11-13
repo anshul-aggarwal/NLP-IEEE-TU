d730ba83cdb99c7a88a64f51ccb8c7a99e2e6af8
Anshul Aggarwal <anshulaggarwal@outlook.com>
2017-11-13 17:34:20 +0530

    Create parsing.py

---
+++ Sample Code/parsing.py
@@ -0,0 +1,20 @@
+import nltk
+from nltk import CFG
+
+grammar1 = CFG.fromstring("""
+S -> NP VP
+NP -> Det Nom | PropN
+Nom -> Adj Nom | N
+VP -> V Adj | V NP | V S | V NP PP
+PP -> P NP
+Det -> 'the' | 'an' | 'a'
+N -> 'bear' | 'squirrel' | 'tree'
+Adj -> 'angry' | 'frightened' | 'little' | 'short'
+V -> 'chased'
+P -> 'on' | 'at'
+""")
+
+parser = nltk.RecursiveDescentParser(grammar1)
+sentence1 = "the angry bear chased the frightened little squirrel on the tree".split()
+for t in parser.parse(sentence1):
+    print(t)
